59965cf6e0a94578b9d71da64081b98cab852731
Tushar Rakheja <tusharrakheja@gmail.com>
2017-01-25 17:14:34 -0600

    Adding another exception.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -6,9 +6,11 @@ public class Main {
         try {
             int result = addArguments(args);
             System.out.println(result);
+        } catch (NumberFormatException e) {
+            System.err.println("Please enter valid integers.");
         } catch (Exception e) {
-            System.err.println("Please provide some integers to add");
-        }
+			System.err.println("Some other error occurred.");			
+		}
     }
 
     private static int addArguments(String[] args) {
